3541ba5a7be9e40194f78b3b1e5d5ed11be3a5d5
Bornisha <mudoibornisha@gmail.com>
2025-09-11 10:50:57 +0530

    Add files via upload

---
+++ To display the multiplication table of a given number up to 10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    // Ask the user to enter a number
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "\nMultiplication Table of " << number << ":\n";
+
+    // Generate and display the multiplication table up to 10
+    for (int i = 1; i <= 10; ++i) {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+
+    return 0;
+}
