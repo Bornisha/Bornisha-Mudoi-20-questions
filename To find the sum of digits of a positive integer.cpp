d505ca88e3a24b772bb4176f86a26b9e4eec1a7c
Bornisha <mudoibornisha@gmail.com>
2025-09-13 23:16:57 +0530

    Update To find the sum of digits of a positive integer.cpp

--- To find the sum of digits of a positive integer.cpp
+++ To find the sum of digits of a positive integer.cpp
@@ -1,3 +1,4 @@
+// This code is developed by Bornisha Mudoi
 #include <iostream>
 using namespace std;
 
@@ -16,4 +17,5 @@ int main() {
     cout << "Sum of digits = " << sum << endl;
 
     return 0;
+
 }
